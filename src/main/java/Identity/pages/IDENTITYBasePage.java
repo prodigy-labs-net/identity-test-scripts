ca8f3d69bdf8f589fd38307007c363616e5415e7
arpi8492 <56442144+arpi8492@users.noreply.github.com>
2019-12-04 10:04:47 -0500

    Create IDENTITYBasePage.java

---
+++ src/main/java/Identity/pages/IDENTITYBasePage.java
@@ -0,0 +1,16 @@
+package identity.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import net.prodigylabs.config.ObjectRepository;
+import net.prodigylabs.handlers.ScreenshotHandler;
+import net.prodigylabs.handlers.WebElementHandler;
+
+public class IDENTITYBasePage{
+	
+	protected WebDriver driver = null;
+	private boolean flag = false;
+			
+}
