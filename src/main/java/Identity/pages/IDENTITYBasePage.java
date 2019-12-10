ec2260699ee024dce510244773e6ecaf8f5c1736
arpi8492 <56442144+arpi8492@users.noreply.github.com>
2019-12-10 15:47:53 -0500

    Update IDENTITYBasePage.java

--- src/main/java/Identity/pages/IDENTITYBasePage.java
+++ src/main/java/Identity/pages/IDENTITYBasePage.java
@@ -1,16 +1,16 @@
-package identity.pages;
+package Identity;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import net.prodigylabs.config.ObjectRepository;
-import net.prodigylabs.handlers.ScreenshotHandler;
-import net.prodigylabs.handlers.WebElementHandler;
+import IdentityTestFramework.ObjectRepository;
+import handlers.ScreenshotHandler;
+import handlers.WebElementHandler;
 
-public class IDENTITYBasePage{
+public class IDENTITYBasePage {
 	
 	protected WebDriver driver = null;
 	private boolean flag = false;
-			
+
 }
