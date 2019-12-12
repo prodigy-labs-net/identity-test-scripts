df2c4009f677e9e8b1db19ee4235a80a37e6dc71
arpi8492 <56442144+arpi8492@users.noreply.github.com>
2019-12-12 16:19:18 -0500

    Update DACUI.java

--- src/test/java/stepDefinitions/DACUI.java
+++ src/test/java/stepDefinitions/DACUI.java
@@ -30,43 +30,42 @@ String sName = null;
 	 @After()
 	public void tearDown() throws Exception {		
 		System.out.println("Executing After of Step Defination");
-        Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
+        //Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
 		identitybasetest.driver.quit();
 	}
 	
 	@Given("^User launches the application \"([^\"]*)\"$")
 	public void user_launches_the_application(String url) throws Throwable {
 		identitybasetest.driver.get(url);
-	    Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
+	    //Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
 	}
 
-//	@Given("^user enters text \"([^\"]*)\" in textbox \"([^\"]*)\"$")
-//	public void user_enters_text_in_textbox(String arg1, String arg2) throws Throwable {
-//		identitybasetest.webelementHandler.enterText(ObjectRepository.getobjectLocator(arg2), arg1);
-//		Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));
-//	}
+	@Then("^User enters text \"([^\"]*)\" in textbox \"([^\"]*)\"$")
+	public void user_enters_text_in_textbox(String arg1, String arg2) throws Throwable {
+		identitybasetest.webelementHandler.enterText(ObjectRepository.getobjectLocator(arg2), arg1);
+		//Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));
+	}
 
-	@Given("^user clicks on button \"([^\"]*)\"$")
+	@Given("^User clicks on button \"([^\"]*)\"$")
 	public void user_clicks_on_button(String arg1) throws Throwable {
-		if (arg1.equals("Printed_Signed")) {
-			System.out.println(identitybasetest.webelementHandler.getText("//tr[contains(@id,'documentSigning-table-rows')]"));
-		}
+		//if (arg1.equals("Printed_Signed")) {
+		//	System.out.println(identitybasetest.webelementHandler.getText("//tr[contains(@id,'documentSigning-table-rows')]"));}
 		identitybasetest.webelementHandler.clickButton(ObjectRepository.getobjectLocator(arg1));
-        Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
+       // Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
 	}
 	
 	@Given("^user validates \"([^\"]*)\" with expected value \"([^\"]*)\"$")
 	public void user_validates_with_expected_value(String arg1, String arg2) throws Throwable {
 		System.err.println(identitybasetest.webelementHandler.getText(ObjectRepository.getobjectLocator(arg1)));
 		VerificationHandler.verifyTrue(identitybasetest.webelementHandler.getText(ObjectRepository.getobjectLocator(arg1)).contains(arg2));
-		Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
+		//Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
 	}
 	
-	@Then("^user validates that \"([^\"]*)\" is displayed$")
+	@Then("^validates that \"([^\"]*)\" is displayed$")
 	public void user_validates_that_is_displayed(String element) throws Throwable {
 		Thread.sleep(2000);
 		VerificationHandler.verifyTrue(identitybasetest.webelementHandler.isDisplayed(ObjectRepository.getString(element)));
-		Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName)); 
+		//Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName)); 
 	}
 		
 	
@@ -83,7 +82,7 @@ String sName = null;
     {
        arg1 = arg1*1000;
     	Thread.sleep(arg1);
-		Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
+		//Reporter.addScreenCaptureFromPath(identitybasetest.screenshot.captureScreenShot(sName));  
     } 
     
 	
